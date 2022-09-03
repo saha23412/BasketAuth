fdc9acde783e74ad19d4d79a3b87740c2bb8536d
Alexandr <k.a.r.2002@mail.ru>
2022-09-03 17:38:08 +0300

    Basket

---
+++ src/app/article/index.js
@@ -0,0 +1,50 @@
+import React, { useCallback } from "react";
+import { useStore as useStoreRedux, useSelector as useSelectorRedux, shallowEqual } from "react-redux";
+import useStore from "../../hooks/use-store";
+import useInit from "../../hooks/use-init";
+import { useParams } from "react-router-dom";
+import useTranslate from "../../hooks/use-translate";
+import ArticleCard from "../../components/article-card";
+import Spinner from "../../components/spinner";
+import Layout from "../../components/layout";
+import TopContainer from "../../containers/top";
+import HeadContainer from "../../containers/head";
+import ToolsContainer from "../../containers/tools";
+import actionsArticle from '../../store-redux/article/actions';
+import actionsComments from '../../store-redux/comments/action'
+
+import CommentsWrapper from "../../components/comments/commentsLogic";
+function Article() {
+  const store = useStore();
+  // Параметры из пути /articles/:id
+  const params = useParams();
+  const storeRedux = useStoreRedux();
+  const select = useSelectorRedux(state => ({
+    article: state.article.data,
+    waiting: state.article.waiting,
+  }), shallowEqual);
+  useInit(async () => {
+    //await store.get('article').load(params.id);
+    storeRedux.dispatch(actionsArticle.load(params.id));
+  }, [params.id]);
+  const { t } = useTranslate();
+  const callbacks = {
+    // Добавление в корзину
+    addToBasket: useCallback(_id => store.get('basket').addToBasket(_id), []),
+  };
+
+  return (
+    <Layout>
+      <TopContainer />
+      <HeadContainer title={select.article.title || ''} />
+      <ToolsContainer />
+      <Spinner active={select.waiting}>
+        <ArticleCard article={select.article} onAdd={callbacks.addToBasket} t={t} />
+      </Spinner>
+      <CommentsWrapper
+      />
+    </Layout>
+  )
+}
+
+export default React.memo(Article);
